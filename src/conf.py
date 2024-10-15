e88533f0b6211814a6db6fc526af380caa6a392a
b2200356027 <muhammedtahirakdeniz@gmail.com>
2024-10-15 22:39:59 +0300

    Update Python commands to use python2 for compatibility

--- src/conf.py
+++ src/conf.py
@@ -10,16 +10,16 @@ BENIGN_CLASS_DIR = "./modules/benign_classes"
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 # DEFINED COMMAND: preserve original functionality
-STRING         = "python strp.py -f {1}.apk string -c no;"
-VARIABLE       = "python strp.py -f {1}.apk variable -c no;"
-PCM            = "python pcm.py  -f {1}.apk package -c no;"
-BYTECODE       = "python pcm.py  -f {1}.apk insbyte -c no;" 
-BENIGN_CLASS   = "python pcm.py  -f {1}.apk bclass -c no;"
-RESOURCE_IMAGE = "python res.py  -f {1} image -c no;"
-RESOURCE_XML   = "python res.py  -f {1} resxml -c no -n yes;"
-API_INTER      = "python api.py  -f {1}.apk inter -a android -c no;"
-BEN_PERMISSION = "python api.py  -f {1}.apk bpermission -c no;"
-API_REFLECTION = "python refl.py -f {1}.apk reflect -c no;"
+STRING         = "python2 strp.py -f {1}.apk string -c no;"
+VARIABLE       = "python2 strp.py -f {1}.apk variable -c no;"
+PCM            = "python2 pcm.py  -f {1}.apk package -c no;"
+BYTECODE       = "python2 pcm.py  -f {1}.apk insbyte -c no;" 
+BENIGN_CLASS   = "python2 pcm.py  -f {1}.apk bclass -c no;"
+RESOURCE_IMAGE = "python2 res.py  -f {1} image -c no;"
+RESOURCE_XML   = "python2 res.py  -f {1} resxml -c no -n yes;"
+API_INTER      = "python2 api.py  -f {1}.apk inter -a android -c no;"
+BEN_PERMISSION = "python2 api.py  -f {1}.apk bpermission -c no;"
+API_REFLECTION = "python2 refl.py -f {1}.apk reflect -c no;"
 
 ANTI_DATAFLOW  = "WILL RELEASE AFTER ACADEMIC SUBMISSION PROCESS"
 COMPONENT_DIV  = "WILL RELEASE AFTER ACADEMIC SUBMISSION PROCESS"
@@ -28,14 +28,13 @@ POLY_STR_ENC   = "WILL RELEASE AFTER ACADEMIC SUBMISSION PROCESS"
 
 
 # DESTRUCTIVE OBFUSCATIONS: only for inferring feature's impact
-RM_RESOURCE_PAYLOAD = "python res.py   -f {1} payload -c no;"
-RM_APIS             = "python rmapi.py -f {1}.apk rmall -c no;"
-RM_PERMISSION       = "python api.py   -f {1}.apk permission -c no;"
+RM_RESOURCE_PAYLOAD = "python2 res.py   -f {1} payload -c no;"
+RM_APIS             = "python2 rmapi.py -f {1}.apk rmall -c no;"
+RM_PERMISSION       = "python2 api.py   -f {1}.apk permission -c no;"
 
 
 # Obfuscation Group for individual APK disguise
-OBFUSCATION_LIST = [API_REFLECTION, STRING, VARIABLE,\
-                   RESOURCE_IMAGE+RESOURCE_XML]
+OBFUSCATION_LIST = [PCM]
 #OBFUSCATION_LIST = [STRING]
 #OBFUSCATION_LIST = [API_REFLECTION]
 
